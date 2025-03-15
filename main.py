72d412dce540dcefb4e5937eeae9dd6a054e178f
s-tim-afk <timlin.stephen@yahoo.ie>
2025-03-15 17:44:38 +0000

    added forwards and backwards movement

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import pygame
-from  constants import *
+from constants import *
 from circleshape import *
 from player import *
 
 def main():
-	pygame.init
+	pygame.init()
 	fps_clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 	while True: #game loop
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill((0, 0, 0))
+
 		player.update(dt)
+		screen.fill((0, 0, 0))
 		player.draw(screen)
 		pygame.display.flip()
 		dt = fps_clock.tick(60) / 1000
