9f2c8695a1b08e6f843fb4e5f2ed5a817d712530
s-tim-afk <timlin.stephen@yahoo.ie>
2025-03-15 16:41:05 +0000

    added fps limit

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from  constants import *
 
 def main():
 	pygame.init
+	fps_clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	while True:
+	while True: #game loop
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0, 0, 0))
 		pygame.display.flip()
+		dt = fps_clock.tick(60) / 1000
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
